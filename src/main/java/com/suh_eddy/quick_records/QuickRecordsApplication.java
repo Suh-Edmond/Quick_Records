bc098f42a3610b44d982ecb9502446fbf02df11c
Suh-Edmond <suhedmond25@yahoo.com>
2024-04-03 13:25:42 +0100

    Setup Project

---
+++ src/main/java/com/suh_eddy/quick_records/QuickRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.suh_eddy.quick_records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suh_eddy/quick_records/QuickRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suh_eddy.quick_records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
